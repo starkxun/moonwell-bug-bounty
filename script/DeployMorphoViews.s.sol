73163c2f3384b014830d7bedaeacc7cc1aa72936
Ana Bittencourt <anajuliabit@gmail.com>
2026-01-26 18:14:52 -0300

    update deployer scripts to not use default foundry key

--- script/DeployMorphoViews.s.sol
+++ script/DeployMorphoViews.s.sol
@@ -1,46 +1,36 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.19;
-pragma experimental ABIEncoderV2;
 
 import {console} from "@forge-std/console.sol";
 import {Script} from "@forge-std/Script.sol";
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-import "@forge-std/Test.sol";
+import {Test} from "@forge-std/Test.sol";
 
 import {AllChainAddresses as Addresses} from "@proposals/Addresses.sol";
 import {MorphoViews} from "@protocol/views/MorphoViews.sol";
-import {TransparentUpgradeableProxy, ITransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import {ProxyAdmin} from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
 
-import {console} from "@forge-std/console.sol";
 /*
 to run:
-forge script script/DeployMorphoViews.s.sol:DeployMorphoViews -vvvv --rpc-url {rpc}  --broadcast --etherscan-api-key {key}
-forge script script/DeployMorphoViews.s.sol:DeployMorphoViews -vvvv --rpc-url https://sepolia.base.org --broadcast
+forge script script/DeployMorphoViews.s.sol:DeployMorphoViews -vvvv --rpc-url {rpc} --broadcast --etherscan-api-key {key}
+
+Example for Base mainnet:
+forge script script/DeployMorphoViews.s.sol:DeployMorphoViews -vvvv --rpc-url https://mainnet.base.org --broadcast --verify --etherscan-api-key {BASESCAN_API_KEY}
 
 */
 
 contract DeployMorphoViews is Script, Test {
-    uint256 public PRIVATE_KEY;
-
     Addresses public addresses;
 
     function setUp() public {
         addresses = new Addresses();
-
-        // Default behavior: use Anvil 0 private key
-        PRIVATE_KEY = vm.envOr(
-            "MOONWELL_DEPLOY_PK",
-            77814517325470205911140941194401928579557062014761831930645393041380819009408
-        );
     }
 
     function run() public {
-        vm.startBroadcast(PRIVATE_KEY);
+        vm.startBroadcast();
 
         address unitroller = addresses.getAddress("UNITROLLER");
-        address morpho = 0xce95AfbB8EA029495c66020883F87aaE8864AF92;
+        address morpho = addresses.getAddress("MORPHO_BLUE");
 
         MorphoViews viewsContract = new MorphoViews();
 
@@ -52,11 +42,15 @@ contract DeployMorphoViews is Script, Test {
 
         ProxyAdmin proxyAdmin = new ProxyAdmin();
 
-        new TransparentUpgradeableProxy(
+        TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy(
             address(viewsContract),
             address(proxyAdmin),
             initdata
         );
+
+        console.log("MorphoViews Implementation:", address(viewsContract));
+        console.log("MorphoViews Proxy:", address(proxy));
+        console.log("ProxyAdmin:", address(proxyAdmin));
 
         vm.stopBroadcast();
     }
--- script/DeployMorphoViewsV2.s.sol
+++ script/DeployMorphoViewsV2.s.sol
@@ -20,25 +20,16 @@ forge script script/DeployMorphoViewsV2.s.sol:DeployMorphoViewsV2 -vvvv --rpc-ur
 */
 
 contract DeployMorphoViewsV2 is Script, Test {
-    uint256 public PRIVATE_KEY;
-
     Addresses public addresses;
 
     function setUp() public {
         addresses = new Addresses();
-
-        // Default behavior: use Anvil 0 private key
-        PRIVATE_KEY = vm.envOr(
-            "MOONWELL_DEPLOY_PK",
-            77814517325470205911140941194401928579557062014761831930645393041380819009408
-        );
     }
 
     function run() public {
-        vm.startBroadcast(PRIVATE_KEY);
+        vm.startBroadcast();
 
         address unitroller = addresses.getAddress("UNITROLLER");
-        // Morpho Blue on Base mainnet
         address morpho = addresses.getAddress("MORPHO_BLUE");
 
         MorphoViewsV2 viewsContract = new MorphoViewsV2();

20ccbc8d89b0d5577760ebdba92abe8f13616ab7
Chris <cnobleshriver@gmail.com>
2026-01-27 09:02:39 +0800

    rename to x42

--- proposals/mips/mip-x42/x42.sh
+++ proposals/mips/mip-x42/x42.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export JSON_PATH=proposals/mips/mip-x41/x41.json
-export DESCRIPTION_PATH=proposals/mips/mip-x41/x41.md
+export JSON_PATH=proposals/mips/mip-x42/x42.json
+export DESCRIPTION_PATH=proposals/mips/mip-x42/x42.md
 export PRIMARY_FORK_ID=0
 
 echo "JSON_PATH=$JSON_PATH"

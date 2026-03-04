2b17de2a9ca9cddb0b73e214f78fd18eba65dd98
Ana Bittencourt <anajuliabit@gmail.com>
2026-03-04 20:04:59 -0300

    calculate total ETH value for Moonbeam actions in CrossChainPublishMessageIntegration test

--- test/integration/CrossChainPublishMessageIntegration.t.sol
+++ test/integration/CrossChainPublishMessageIntegration.t.sol
@@ -145,8 +145,19 @@ contract CrossChainPublishMessageTest is Test, PostProposalCheck {
                     );
             }
 
+            // Calculate total ETH value needed for Moonbeam actions
+            uint256 executeValue = 0;
+            {
+                ProposalAction[] memory moonbeamActions = proposal
+                    .getActionsByType(ActionType.Moonbeam);
+                for (uint256 k = 0; k < moonbeamActions.length; k++) {
+                    executeValue += moonbeamActions[k].value;
+                }
+            }
+            vm.deal(address(this), executeValue);
+
             vm.recordLogs();
-            governor.execute(proposalId);
+            governor.execute{value: executeValue}(proposalId);
             Vm.Log[] memory logs = vm.getRecordedLogs();
 
             bytes32 sig = keccak256(
